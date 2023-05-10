246ae1f5aede92c84e6699ce19915dda5fa595cb
Draxler1902 <agrigoryev13@yandex.ru>
2023-05-11 01:14:26 +0300

    TaskController

--- ToDoList.Domain/Enum/Priority.cs
+++ ToDoList.Domain/Enum/Priority.cs
@@ -1,8 +1,12 @@
-namespace ToDoList.Domain.Enum;
+using System.ComponentModel.DataAnnotations;
 
+namespace ToDoList.Domain.Enum;
 public enum Priority
 {
+    [Display(Name = "Простая")]
     Easy = 1,
+    [Display(Name = "Средняя")]
     Medium = 2,
+    [Display(Name = "Сложная")]
     Hard = 3
 }
