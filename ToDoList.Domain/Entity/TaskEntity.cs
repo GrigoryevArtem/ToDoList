e467efc9067063ccb1c6c156abcfd048f273ec6b
Draxler1902 <agrigoryev13@yandex.ru>
2023-05-11 00:48:49 +0300

    Change model's properties

--- ToDoList.Domain/Entity/TaskEntity.cs
+++ ToDoList.Domain/Entity/TaskEntity.cs
@@ -8,5 +8,8 @@ public class TaskEntity
     public long Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public DateTime Created { get; set; }
     public Priority Priority { get; set; }
+
+    public bool IsDone { get; set; }
 }
--- ToDoList.Domain/Enum/StatusCode.cs
+++ ToDoList.Domain/Enum/StatusCode.cs
@@ -2,6 +2,7 @@ namespace ToDoList.Domain.Enum;
 
 public enum StatusCode
 {
-    Ok = 200,
+    TaskIsHasAlready = 1,
+    OK = 200,
     InternalServerError = 500
 }
